d9bcb54a0da8fc368cec22c91cfc07c938951d87
Kiman Park <kimanpark33@gmail.com>
2025-02-06 00:32:43 -0500

    modified

--- convertMySQLtoPostgreSQL.py
+++ convertMySQLtoPostgreSQL.py
@@ -12,13 +12,16 @@ import re
 import pandas as pd
 from sqlalchemy import create_engine
 
+# Loading the environment variables
 load_dotenv('.env')
 
 # =============================================================================
-# Connect and get the data from the local database, which is in the format of MySQL
+# Connect and get the data from the local database, which is in the format of MySQL, which we can
+# using sqlite3
 local_db = r'C:\Users\brian\OneDrive - University of Tennessee\Desktop\Research\Python program\Springboard\SQLFiles-Tier-2-Unit-8\sqlite_db_pythonsqlite.db'
 sqlite_con = sqlite3.connect(local_db)
 sqlite_cur = sqlite_con.cursor()
+
 # getting tables
 sqlite_cur.execute("SELECT name FROM sqlite_master WHERE type='table'")
 tables = sqlite_cur.fetchall()
@@ -27,6 +30,7 @@ print('*'*50)
 print('TABLES:', tables)
 print('\n\n')
 print('*'*50)
+
 tables_columns = {}
 column_types = {}
 for table in tables:
@@ -48,18 +52,18 @@ for table in tables:
     for description in describe:
         column_types[description[1]] = description[2]
     
-    
+#### We do not need this anymore. ####
 # Getting ready for the conversion into Postgresql from sqlite
 # The decimal is numeric
 # The int(n,m) is now just int
-for col,types in column_types.items():
-    if re.match('int',types):
-        column_types[col] = 'INT'
-    elif re.match('decimal',types):
-        types = types.split('(')[-1].split(')')[0]
-        column_types[col] = f'NUMERIC({types})'
-    else:
-        pass
+# for col,types in column_types.items():
+#     if re.match('int',types):
+#         column_types[col] = 'INT'
+#     elif re.match('decimal',types):
+#         types = types.split('(')[-1].split(')')[0]
+#         column_types[col] = f'NUMERIC({types})'
+#     else:
+#         pass
 
 print(column_types)
 
@@ -77,7 +81,8 @@ for key, value in tables_columns.items():
         print(i+1, v, ',',  f'{column_types[v]}')
     print('\n')
     print('-'*20)
-print(f'Columns Types: {len(column_types)}', column_types)
+print(f'Total Columns Names and Types: {len(column_types)}', column_types)
+print('\n')
 # =============================================================================
 
 
@@ -93,16 +98,16 @@ postgreSQL_url = f'postgresql://{os.getenv("user")}:{os.getenv("password")}@{os.
 pg_conn = create_engine(postgreSQL_url)
 
 
-# Using pandas 
-
+# Using pandas!!! Much easier and faster honestly. 
 for table in tables:    
     table_split = [t for t in re.split("([A-Z][^A-Z]*)", table[0]) if t]
     table_split = '_'.join(table_split)
     table_split = table_split.lower()
     df = pd.read_sql(f'SELECT * FROM {table[0]}', con=sqlite_con)
-    print(df.head())
-    df.to_sql(name=table_split, con=pg_conn, chunksize=5000, index=False, index_label=False, if_exists='replace')    
-    
+    df.to_sql(name=table_split, con=pg_conn, chunksize=5000, index=False, index_label=False, if_exists='replace')   
+print('*'*30)
+print('Successfully Migrated!')
+print('*'*30)
 # =============================================================================
 # pg_cur.execute(
 #     """
